071ba3745ad65798ed9152a7a71294e878faf607
besh612 <sung.hun5016@gmail.com>
2019-10-16 23:38:53 +0900

    add BaseView layout

--- pos-app/src/main/java/com/noqueue/pos/PosMain.java
+++ pos-app/src/main/java/com/noqueue/pos/PosMain.java
@@ -1,8 +1,24 @@
 package com.noqueue.pos;
 
+import com.noqueue.pos.ui.BaseView;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class PosMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+		throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
 		System.out.println("Pos Main");
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				BaseView bv = new BaseView();
+				bv.setVisible(true);
+			}
+		});
+
 	}
 }
