c58a93fc7ba2c1b3f1b29afb7b73e8139bfc7749
besh612 <sung.hun5016@gmail.com>
2019-11-05 20:27:36 +0900

    add json DB and parser

---
+++ pos-app/src/main/java/com/noqueue/pos/utils/JsonParser.java
@@ -0,0 +1,20 @@
+package com.noqueue.pos.utils;
+
+import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JsonParser {
+
+	public static JSONArray getDataList() throws Exception {
+		JSONParser jsonParser = new JSONParser();
+		Reader reader = new FileReader("pos-app/src/main/resources/db.json");
+		JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+		return (JSONArray) jsonObject.get("lists");
+	}
+
+	public static void main(String[] args) throws Exception {
+		getDataList();
+	}
+}
